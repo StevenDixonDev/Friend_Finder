5a7682798a3d221ea6c355f1e1a31c30ff2ff293
StevenDixonDev <stevedixon1128.dev@gmail.com>
2019-09-04 16:42:09 -0400

    working on matching algorithm

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,16 +10,43 @@ router.get('/', (req, res)=>{
 
 // get previous matches
 router.get('/previous', (req, res)=> {
-  console.log(previous)
+  // send previous matched to the page
   res.json(JSON.stringify(previous));
 })
 
 // update app from survey page
 router.post('/', (req, res)=>{
-  console.log(req.body);
-
-  res.json(friends[0]);
+  // create a user
+  let user = processData(req.body);
+  // get a match based on user
+  let match = getMatch(req.body);
+  // add user to friends list
+  friends.push(user);
+  // add match to previous matched list
+  previous.push({name: user.name, match: match.name});
+  // send match back to front end
+  res.json(match);
 })
 
 
 module.exports = router;
+
+function processData(data){
+  let user = {}
+  // assign a name
+  user['name'] = data.userName;
+  // assign a photo
+  user['photo'] = data.pictureName;
+  // create an array of user responses
+  let arr = [];
+  for(let i = 1; i < 11; i++){
+    arr.push(data[i]);
+  }
+  user["scores"] = arr;
+  return user;
+}
+
+
+function getMatch(data){
+  
+}
